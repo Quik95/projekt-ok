9a45a1ba330b9699404a6bc286212a17d93a4c44
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2022-10-24 18:53:15 +0200

    Initial commit

---
+++ GreedyHeuristic/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace OK;
+
+public record struct Processor(string ID, List<int> Tasks)
+{
+    public int TotalTime() => Tasks.Sum();
+
+    public void AssignTask(int task)
+    {
+        Tasks.Add(task);
+    }
+}
+
+interface ITaskSchedulingSolver
+{
+    public Processor[] Solve();
+}
+
+public sealed class GreedyHeuristic : ITaskSchedulingSolver
+{
+    private int[] Instance;
+    private Processor[] ParallelProcessors;
+
+    public GreedyHeuristic(int numberOfProcessors, int[] instance)
+    {
+        Instance = instance;
+        ParallelProcessors = Enumerable.Range(0, numberOfProcessors).Select(id => new Processor(id.ToString(), new List<int>())).ToArray();
+    }
+
+    public GreedyHeuristic(string filePath)
+    {
+        var lines = File.ReadAllLines(filePath);
+        var n = int.Parse(lines.First());
+        
+        Instance = lines.Skip(2).Select(int.Parse).ToArray();
+        ParallelProcessors = Enumerable.Range(0, n).Select(id => new Processor(id.ToString(), new List<int>())).ToArray();
+    }
+
+    public Processor[] Solve()
+    {
+        var queue = new PriorityQueue<Processor, int>(ParallelProcessors.Select(p => (p, 0)), Comparer<int>.Create((lhs, rhs) => lhs - rhs));
+        queue.EnsureCapacity(ParallelProcessors.Length);
+
+        foreach (var task in Instance)
+        {
+            var top = queue.Dequeue();
+            top.AssignTask(task);
+            queue.Enqueue(top, top.TotalTime());
+        }
+
+        return queue.UnorderedItems.Select(p => p.Element).ToArray();
+    }
+}
+
+internal static class Program
+{
+    public static void Main()
+    {
+        // var greedySolver = new GreedyHeuristic(2, new []{5, 4, 3, 2, 1});
+        var greedySolver = new GreedyHeuristic("test_instance.txt");
+        var solvedInstance = greedySolver.Solve();
+        var sorted = solvedInstance.ToList();
+        sorted.Sort((lhs, rhs) => lhs.TotalTime() - rhs.TotalTime());
+        foreach (var processor in sorted)
+        {
+            Console.WriteLine($"{processor} => PCmax: {processor.TotalTime()} | Tasks: {processor.Tasks.Count}");
+        }
+    }
+}
---
+++ InstanceGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.Serialization;
+
+const int minTaskLength = 10;
+const int maxTaskLength = 90;
+
+var numberOfParallelProcessors = Random.Shared.Next(1, 31);
+var numberOfTasks = Random.Shared.Next(numberOfParallelProcessors + 1, 301);
+var tasks = Enumerable.Range(0, numberOfTasks).Select(_ => Random.Shared.Next(minTaskLength, maxTaskLength+1));
+using var file = File.CreateText("instance.txt");
+
+file.WriteLine(numberOfParallelProcessors);
+file.WriteLine(numberOfTasks);
+foreach (var task in tasks)
+{
+    file.WriteLine(task);
+}
